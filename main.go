b2a1cb81e13076c60d120dbc7a5fb3ba58c95ea0
whywaita <whywaita@whywrite.it>
2023-05-04 20:39:29 +0900

    Use CamelCase and const type

--- main.go
+++ main.go
@@ -71,56 +71,62 @@ func (p *packet) init() error {
 	return nil
 }
 
-func (p *packet) setcommandtype() ([]byte, error) {
+func (p *packet) setCommandType() ([]byte, error) {
 	cmd := []byte{0x2A, 0x01}
 	return p.write(cmd)
 }
 
-func (p *packet) switch_rf() ([]byte, error) {
+func (p *packet) switchRF() ([]byte, error) {
 	cmd := []byte{0x06, 0x00}
 	return p.write(cmd)
 }
 
-// inset_rf set a bitrate of RF communication
-func (p *packet) inset_rf(nfc_type byte) ([]byte, error) {
+// inSetRF set a bitrate of RF communication
+func (p *packet) inSetRF(nfcType NFCType) ([]byte, error) {
 	var cmd []byte
-	switch nfc_type {
-	case 'F':
+	switch nfcType {
+	case NFCTypeF:
 		cmd = []byte{InSetRf, 0x01, 0x01, 0x0f, 0x01} // 212F
-	case 'A':
+	case NFCTypeA:
 		cmd = []byte{InSetRf, 0x02, 0x03, 0x0f, 0x03} // 106A
-	case 'B':
+	case NFCTypeB:
 		cmd = []byte{InSetRf, 0x03, 0x07, 0x0f, 0x07} // 106B
 	}
 	return p.write(cmd)
 }
 
-func (p *packet) inset_protocol_1() ([]byte, error) {
+func (p *packet) inSetProtocol1() ([]byte, error) {
 	cmd := []byte{InSetProtocol, 0x00, 0x18, 0x01, 0x01, 0x02, 0x01, 0x03, 0x00, 0x04, 0x00, 0x05, 0x00, 0x06, 0x00, 0x07, 0x08, 0x08, 0x00, 0x09, 0x00, 0x0a, 0x00, 0x0b, 0x00, 0x0c, 0x00, 0x0e, 0x04, 0x0f, 0x00, 0x10, 0x00, 0x11, 0x00, 0x12, 0x00, 0x13, 0x06}
 	return p.write(cmd)
 }
 
-func (p *packet) inset_protocol_2(nfc_type byte) ([]byte, error) {
+func (p *packet) inSetProtocol2(nfcType NFCType) ([]byte, error) {
 	var cmd []byte
-	switch nfc_type {
-	case 'F':
+	switch nfcType {
+	case NFCTypeF:
 		cmd = []byte{InSetProtocol, 0x00, 0x18}
-	case 'A':
-		cmd = []byte{InSetProtocol, 0x01, 0x00, 0x02, 0x00, 0x05, 0x01, 0x00, 0x06, 0x07, 0x07}
-	case 'B':
+	case NFCTypeA:
+		return p.inSetProtocol(map[string]int{
+			"initial_guard_time":  6,
+			"add_crc":             0,
+			"check_crc":           0,
+			"check_parity":        1,
+			"last_byte_bit_count": 7,
+		})
+	case NFCTypeB:
 		cmd = []byte{InSetProtocol, 0x00, 0x14, 0x09, 0x01, 0x0a, 0x01, 0x0b, 0x01, 0x0c, 0x01}
 	}
 	return p.write(cmd)
 }
 
-func (p *packet) sens_req(nfc_type byte) ([]byte, error) {
+func (p *packet) sensReq(nfcType NFCType) ([]byte, error) {
 	var cmd []byte
-	switch nfc_type {
-	case 'F':
+	switch nfcType {
+	case NFCTypeF:
 		cmd = []byte{InCommRf, 0x6e, 0x00, 0x06, 0x00, 0xff, 0xff, 0x01, 0x00}
-	case 'A':
+	case NFCTypeA:
 		cmd = []byte{InCommRf, 0x36, 0x01, 0x26}
-	case 'B':
+	case NFCTypeB:
 		cmd = []byte{InCommRf, 0x6e, 0x00, 0x05, 0x00, 0x10}
 	}
 
@@ -128,7 +134,7 @@ func (p *packet) sens_req(nfc_type byte) ([]byte, error) {
 }
 
 func (p *packet) parse(buf []byte) []byte {
-	return buf[9:len(buf)]
+	return buf[9:]
 }
 
 func newPacket(ctx *gousb.Context, dev *gousb.Device) (*packet, error) {
@@ -168,6 +174,10 @@ func newPacket(ctx *gousb.Context, dev *gousb.Device) (*packet, error) {
 }
 
 func GetID(vid, pid uint16) ([]byte, error) {
+	return GetIDWithType(vid, pid, NFCTypeF)
+}
+
+func GetIDWithType(vid, pid uint16, in NFCType) ([]byte, error) {
 	ctx := gousb.NewContext()
 	defer ctx.Close()
 
@@ -177,10 +187,60 @@ func GetID(vid, pid uint16) ([]byte, error) {
 	}
 	defer dev.Close()
 
-	return GetIDByDevice(ctx, dev)
+	return GetIDByDevice(ctx, dev, in)
 }
 
-func GetIDByDevice(ctx *gousb.Context, dev *gousb.Device) ([]byte, error) {
+type NFCType int
+
+const (
+	NFCTypeUnknown NFCType = iota
+	NFCTypeA
+	NFCTypeB
+	NFCTypeF
+)
+
+func (t NFCType) String() string {
+	switch t {
+	case NFCTypeA:
+		return "A"
+	case NFCTypeB:
+		return "B"
+	case NFCTypeF:
+		return "F"
+	default:
+		return "Unknown"
+	}
+}
+
+func UnmarshalNFCType(in any) NFCType {
+	switch v := in.(type) {
+	case string:
+		return UnmarshalNFCTypeString(v)
+	case NFCType:
+		return in.(NFCType)
+	default:
+		return NFCTypeUnknown
+	}
+}
+
+func UnmarshalNFCTypeString(in string) NFCType {
+	switch in {
+	case "A":
+		return NFCTypeA
+	case "B":
+		return NFCTypeB
+	case "F":
+		return NFCTypeF
+	default:
+		return NFCTypeUnknown
+	}
+}
+
+func GetIDByDevice(ctx *gousb.Context, dev *gousb.Device, nfcType NFCType) ([]byte, error) {
+	if nfcType == NFCTypeUnknown {
+		return nil, errors.New("unknown nfc type")
+	}
+
 	p, err := newPacket(ctx, dev)
 	if err != nil {
 		return nil, err
@@ -191,36 +251,34 @@ func GetIDByDevice(ctx *gousb.Context, dev *gousb.Device) ([]byte, error) {
 		return nil, err
 	}
 
-	_, err = p.setcommandtype()
+	_, err = p.setCommandType()
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = p.switch_rf()
+	_, err = p.switchRF()
 	if err != nil {
 		return nil, err
 	}
 
-	var nfc_type byte
-	nfc_type = 'A'
-	_, err = p.inset_rf(nfc_type)
+	_, err = p.inSetRF(nfcType)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = p.inset_protocol_1()
+	_, err = p.inSetProtocol1()
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = p.inset_protocol_2(nfc_type)
+	_, err = p.inSetProtocol2(nfcType)
 	if err != nil {
 		return nil, err
 	}
 
 	isloop := true
 	for isloop {
-		rbuf, err := p.sens_req(nfc_type)
+		rbuf, err := p.sensReq(nfcType)
 		if err != nil {
 			return nil, err
 		}
@@ -264,14 +322,14 @@ func GetIDByDevice(ctx *gousb.Context, dev *gousb.Device) ([]byte, error) {
 }
 
 func (p *packet) getTypeAUID() ([]byte, error) {
-	if _, err := p.inset_protocol(map[string]int{
+	if _, err := p.inSetProtocol(map[string]int{
 		"last_byte_bit_count": 8,
 		"add_parity":          1,
 	}); err != nil {
 		return nil, err
 	}
 
-	if _, err := p.inset_protocol(map[string]int{
+	if _, err := p.inSetProtocol(map[string]int{
 		"add_crc":   1,
 		"check_crc": 1,
 	}); err != nil {
@@ -279,41 +337,40 @@ func (p *packet) getTypeAUID() ([]byte, error) {
 	}
 
 	uid := make([]byte, 0)
-	for _, sel_cmd := range []byte{0x93, 0x95, 0x97} {
-		if _, err := p.inset_protocol(map[string]int{
+	for _, selCmd := range []byte{0x93, 0x95, 0x97} {
+		if _, err := p.inSetProtocol(map[string]int{
 			"add_crc":   0,
 			"check_crc": 0,
 		}); err != nil {
 			return nil, err
 		}
 
-		sdd_req := []byte{sel_cmd, 0x20}
-		sdd_res, err := p.inCommRf(sdd_req)
+		sddReq := []byte{selCmd, 0x20}
+		sddRes, err := p.inCommRf(sddReq)
 		if err != nil {
 			return nil, err
 		}
-		sdd_res = sdd_res[13 : 13+5]
-		if _, err := p.inset_protocol(map[string]int{
+		sddRes = sddRes[13 : 13+5]
+		if _, err := p.inSetProtocol(map[string]int{
 			"add_crc":   1,
 			"check_crc": 1,
 		}); err != nil {
 			return nil, err
 		}
 
-		sel_req := []byte{sel_cmd, 0x70}
-		for _, b := range sdd_res {
-			sel_req = append(sel_req, b)
-		}
-		sel_res, err := p.inCommRf(sel_req)
+		selReq := []byte{selCmd, 0x70}
+		selReq = append(selReq, sddRes...)
+
+		selRes, err := p.inCommRf(selReq)
 		if err != nil {
 			return nil, err
 		}
-		sel_res = []byte{sel_res[13]}
+		selRes = []byte{selRes[13]}
 
-		if sel_res[0]&0b00000100 != 0 {
-			uid = append(uid, sdd_res[1:4]...)
+		if selRes[0]&0b00000100 != 0 {
+			uid = append(uid, sddRes[1:4]...)
 		} else {
-			uid = append(uid, sdd_res[0:4]...)
+			uid = append(uid, sddRes[0:4]...)
 			break
 		}
 	}
@@ -323,9 +380,7 @@ func (p *packet) getTypeAUID() ([]byte, error) {
 
 func (p *packet) inCommRf(data []byte) ([]byte, error) {
 	cmd := []byte{InCommRf, 0x36, 0x01}
-	for _, d := range data {
-		cmd = append(cmd, d)
-	}
+	cmd = append(cmd, data...)
 
 	b, err := p.write(cmd)
 	if err != nil {
@@ -339,23 +394,14 @@ func (p *packet) inCommRf(data []byte) ([]byte, error) {
 	return b[2:], nil
 }
 
-func (p *packet) inset_protocol(kwargs map[string]int) ([]byte, error) {
+func (p *packet) inSetProtocol(kwargs map[string]int) ([]byte, error) {
 	data := inSetProtocol(kwargs)
 	cmd := []byte{InSetProtocol}
-	for _, d := range data {
-		cmd = append(cmd, d)
-	}
+	cmd = append(cmd, data...)
 
 	return p.write(cmd)
 }
 
-type StatusError struct {
-	Code int
-}
-
-func (e *StatusError) Error() string {
-	return fmt.Sprintf("Status error: %d", e.Code)
-}
 func inSetProtocol(kwargs map[string]int) []byte {
 	data := make([]byte, 0)
 
